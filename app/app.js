e56761129b9d632a5ff766611de6af8f539ce5aa
Riccardo Bartoli <info@rblab.com>
2016-01-12 15:04:13 +0000

    Refactored App location

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, Link} from 'react-router';
-import App from './views/app';
+import App from './components/app';
 
 ReactDOM.render((
     <Router>
