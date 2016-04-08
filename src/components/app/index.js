9c156e2c63436e1dee7d73f0dd760ff4195d696b
Riccardo Bartoli <info@rblab.com>
2016-04-09 00:03:08 +0100

    Update react and react-dom to 15.0.1

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -10,7 +10,7 @@ class App extends Component {
                 <ul>
                     <li><Link to="/">{'Home'}</Link></li>
                 </ul>
-                
+
                 {this.props.children}
             </div>
         )
